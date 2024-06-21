eb1e2af3d9823b016711ccb8d5fadb497e3d2129
Redox <mail@redox.to>
2024-06-21 23:36:16 +0200

    Improve hookperf

--- lua/autorun/sh_hookperf.lua
+++ lua/autorun/sh_hookperf.lua
@@ -1,6 +1,27 @@
+local hookC = Color( 58, 150, 221 )
+local darkGray = Color( 100, 100, 100 )
+local softWhite = Color( 205, 205, 205 )
+local hookIDC = Color( 19, 161, 14 )
+local timeC = Color( 231, 72, 86 )
+local countC = Color( 52, 64, 235 )
+
+local colors = { hookIDC, hookC, timeC, countC, softWhite, darkGray }
+local function printer( ... )
+    local order = {}
+    for i, txt in pairs( { ... } ) do
+        table.insert( order, colors[i] )
+        table.insert( order, txt .. " " )
+    end
+
+    table.insert( order, "\n" )
+
+    MsgC( unpack( order ) )
+end
+
 local cmd = SERVER and "red_sv_hookperf" or "red_cl_hookperf"
-concommand.Add( cmd, function( ply )
+concommand.Add( cmd, function( ply, _, args )
     if SERVER and IsValid( ply ) then return end
+    local time = tonumber( args[1] ) or 10
 
     local lagTbl = {}
     HOOK_PERF_ORIGINALS = HOOK_PERF_ORIGINALS or {}
@@ -11,10 +32,14 @@ concommand.Add( cmd, function( ply )
             HOOK_PERF_ORIGINALS[hookName][hookEvent] = hookFunc
 
             local hookFunc = hookFunc
+            local originInfo = debug.getinfo( hookFunc, "S" )
+            local hookFuncOrigin = originInfo.short_src
+            local hookFuncLastDefined = originInfo.lastlinedefined
+
             local function wrapper( ... )
                 local info = lagTbl[hookEvent]
                 if not info then
-                    info = { count = 0, time = 0, hook = hookName }
+                    info = { count = 0, time = 0, hook = hookName, origin = hookFuncOrigin, lastDefined = hookFuncLastDefined }
                     lagTbl[hookEvent] = info
                 end
                 info.count = info.count + 1
@@ -30,7 +55,7 @@ concommand.Add( cmd, function( ply )
         end
     end
 
-    timer.Simple( 10, function()
+    timer.Simple( time, function()
         -- restore
         for hookName, hookTable in ipairs( hook.GetTable() ) do
             for hookEvent in pairs( hookTable ) do
@@ -49,12 +74,13 @@ concommand.Add( cmd, function( ply )
             return a[2].time > b[2].time
         end )
 
-        print( "Laggy hooks:" )
+        MsgC( softWhite, "Laggy hooks:\n" )
+        printer( "Hook name", "Hook", "Time", "Count", "Origin", "Line defined" )
         for i = 1, 100 do
             local v = sorted[i]
             if not v then break end
 
-            print( v[1], v[2].hook, v[2].time, v[2].count )
+            printer( v[1], v[2].hook, v[2].time, v[2].count, v[2].origin, v[2].lastDefined )
         end
 
         HOOK_PERF_ORIGINALS = nil
