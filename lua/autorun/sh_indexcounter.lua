2268b15e0a8be84efaa914932105e2eb630c6e7d
Redox <mail@redox.to>
2024-06-21 23:45:08 +0200

    Improve index counter reach

--- lua/autorun/sh_indexcounter.lua
+++ lua/autorun/sh_indexcounter.lua
@@ -1,24 +1,38 @@
-local meta = FindMetaTable( "Entity" )
+local entMeta = FindMetaTable( "Entity" )
+local plyMeta = FindMetaTable( "Player" )
+local wepMeta = FindMetaTable( "Weapon" )
 
-ENT_INDEX = ENT_INDEX or meta.__index
+ENT_INDEX = ENT_INDEX or entMeta.__index
+PLAYER_INDEX = PLAYER_INDEX or plyMeta.__index
+WEAPON_INDEX = WEAPON_INDEX or wepMeta.__index
+
+local toWrap = {
+    [entMeta] = ENT_INDEX,
+    [plyMeta] = PLAYER_INDEX,
+    [wepMeta] = WEAPON_INDEX
+}
 
 local cmd = SERVER and "red_sv_indexcounter" or "red_cl_indexcounter"
-concommand.Add( cmd, function( ply )
+concommand.Add( cmd, function( ply, _, args )
     if SERVER and IsValid( ply ) then return end
+    local time = tonumber( args[1] ) or 10
 
     local origins = {}
 
-    function meta.__index( tbl, key )
+    for meta, original in pairs( toWrap ) do
+        meta.__index = function( tbl, key )
+            local info = debug.getinfo( 2 )
+            local name = info.short_src .. ":" .. info.linedefined
+            origins[name] = origins[name] and origins[name] + 1 or 1
 
-        local info = debug.getinfo( 2 )
-        local name = info.short_src .. ":" .. info.linedefined
-        origins[name] = origins[name] and origins[name] + 1 or 1
-
-        return ENT_INDEX( tbl, key )
+            return original( tbl, key )
+        end
     end
 
-    timer.Simple( 10, function()
-        meta.__index = ENT_INDEX
+    timer.Simple( time, function()
+        entMeta.__index = ENT_INDEX
+        plyMeta.__index = PLAYER_INDEX
+        wepMeta.__index = WEAPON_INDEX
 
         local indexed = {}
         for origin, count in pairs( origins ) do
@@ -27,7 +41,11 @@ concommand.Add( cmd, function( ply )
 
         table.sort( indexed, function( a, b ) return a.count > b.count end )
 
+        local max = 100
         for _, data in ipairs( indexed ) do
+            max = max - 1
+            if max < 0 then break end
+
             print( data.count, data.origin )
         end
 
