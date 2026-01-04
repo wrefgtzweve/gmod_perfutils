6e1de0358860368b1775b4a998474b0907ee9980
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2026-01-04 08:54:22 +0300

    Replace bytes to bits
    This really confused me until i looked at the code

--- lua/autorun/sh_net_logger.lua
+++ lua/autorun/sh_net_logger.lua
@@ -84,7 +84,7 @@ local function applyWrap()
         end
 
         if not ignoreNets[strName] then
-            MsgC( startColor, "NET START: ", realmColor, strName, blueColor, " (" .. len .. " bytes)", "\n" )
+            MsgC( startColor, "NET START: ", realmColor, strName, blueColor, " (" .. len .. " bits)", "\n" )
             wrapNetRead()
         end
 
@@ -143,4 +143,5 @@ concommand.Add( SERVER and "red_sv_netlogger_ignore" or "red_cl_netlogger_ignore
 
     ignoreNets[name] = true
     print( "Ignoring net message: " .. name )
+
 end )
